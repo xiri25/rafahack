8f5e8f0777923f0347843ffe516208075923287e
xiri25 <86271178+xiri25@users.noreply.github.com>
2024-02-09 09:55:27 +0100

    Update rafa.sql

--- Server/src/rafa.sql
+++ Server/src/rafa.sql
@@ -1,10 +1,10 @@
--- Crear la base de datos si no existe
+
 CREATE DATABASE IF NOT EXISTS rafa;
 
--- Usar la base de datos
+
 USE rafa;
 
--- Crear la tabla "victimas"
+
 CREATE TABLE victimas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     victima_key VARCHAR(50),
